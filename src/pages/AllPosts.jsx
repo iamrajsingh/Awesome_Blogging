4c2caf7a9c4ddd9c2106ae0698e56c89410d7139
iamrajsingh <divyraj65407@gmail.com>
2023-10-10 00:01:00 +0530

    All post ui improved

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -13,11 +13,11 @@ const AllPosts = () => {
   }, [posts]);
 
   return (
-    <div className="relative w-full text-center bg-secondary h-screen ">
+    <div className="w-full text-center bg-secondary h-screen ">
       <Container>
-        <div className="relative flex flex-wrap pt-[136px] h-full">
+        <div className="flex flex-wrap pt-[136px] h-full">
           {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
+            <div key={post.$id} className="p-2 w-1/4 max-sm:w-full max-md:w-1/2">
               <PostCard {...post} />
             </div>
           ))}
